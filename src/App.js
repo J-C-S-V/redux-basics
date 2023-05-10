11031be2128d999d0eb2811761128fab2152fef2
Juan Sanchez <juansan1993@hotmail.com>
2023-05-09 22:43:31 -0500

    Add reducers and logic for counters and total

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Navbar from './components/Navabar'
+import Navbar from './components/Navabar';
 import CartContainer from './components/CartContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import { useEffect } from 'react';
 
 function App() {
-  return (<main>
-    <Navbar />
-    <CartContainer />
-  </main>
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 export default App;
--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,18 +1,20 @@
+import { clearCart } from '../features/cart/cartSlice';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const CartContainer = () => {
-  const { cartItems, joder, joder2 } = useSelector((store) => store.cart)
-  console.log(cartItems, joder, joder2);
+  const dispatch = useDispatch();
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
 
-
-  if(joder2 < 1){
-    return <section className="cart">
-      <header>
-        <h2>your bag</h2>
-        <h4 className="empty-cart">is currently empty</h4>
-      </header>
-    </section>
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
   }
   return (
     <section className="cart">
@@ -20,19 +22,23 @@ const CartContainer = () => {
         <h2>your bag</h2>
       </header>
       <div>
-        {cartItems.map((item => {
-          return <CartItem key={item.id} {...item} />
-        }))}
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
       </div>
       <footer>
         <hr />
-        <div className="cart-joder">
-         <h4>joder<span>${joder}</span></h4>
+        <div className="cart-total">
+          <h4>
+            total<span>${total.toFixed(2)}</span>
+          </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
-  )
-}
+  );
+};
 
 export default CartContainer;
